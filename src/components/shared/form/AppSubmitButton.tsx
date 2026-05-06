09a7bb273e5631049275318d1be959b4c7c0a259
Saidur289 <saidur.riaz1@gmailcomm>
2026-05-06 12:44:54 +0600

    create reuseable form and submit button

---
+++ src/components/shared/form/AppSubmitButton.tsx
@@ -0,0 +1,39 @@
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { Loader2 } from "lucide-react";
+
+type AppSubmitButtonProps = {
+  isPending?: boolean;
+  children: React.ReactNode;
+  pendingLabel?: string;
+  className?: string;
+  disabled?: boolean;
+};
+
+const AppSubmitButton = ({
+  isPending,
+  children,
+  pendingLabel = "Submitting...",
+  className,
+  disabled = false,
+}: AppSubmitButtonProps) => {
+  const isDisabled = isPending || disabled;
+  return (
+    <Button
+      type="submit"
+      className={cn("w-full", className)}
+      disabled={isDisabled}
+    >
+      {isPending ? (
+        <>
+          {" "}
+          <Loader2 className="animate-spin" aria-hidden="true" />
+          {pendingLabel ? pendingLabel : children}{" "}
+        </>
+      ) : (
+        children
+      )}
+    </Button>
+  );
+};
+export default AppSubmitButton;
